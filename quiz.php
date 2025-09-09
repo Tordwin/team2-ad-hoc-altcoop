d1e33474a81b1d397591a61fc274189f29fc54f7
Tordwin <ec7233@g.rit.edu>
2025-09-09 15:18:35 -0400

    Creation of template for Sr Dev Web Assessment

---
+++ quiz.php
@@ -0,0 +1,119 @@
+<html>
+    <head>
+        <title>Senior Dev Self-Assessment Web-Form</title>
+    </head>
+    <body>
+        <form method="POST">
+            <label>Name</label><br>
+            <input type="text" name="name" required><br><br>
+
+            <label>Email</label><br>
+            <input type="text" name="email" required><br><br>
+
+            <label>Major</label><br>
+            <input type="text" name="major" required><br><br>
+
+            <label>Course ID</label><br>
+            <input type="text" name="course_id" required><br><br>
+
+            <label>Date</label><br>
+            <input type="date" name="date" required><br><br>
+
+            <h3>Open Ended Questions</h3>
+
+            <label>How well is your front-end development skills?</label><br>
+            <input type="radio" name="open_ended_1_1" value="Beginner"> Beginner
+            <input type="radio" name="open_ended_1_2" value="Intermediate"> Intermediate
+            <input type="radio" name="open_ended_1_3" value="Advanced"> Advanced<br><br>
+            
+            <label>How well is your back-end development skills?</label><br>
+            <input type="radio" name="open_ended_2_1" value="Beginner"> Beginner
+            <input type="radio" name="open_ended_2_2" value="Intermediate"> Intermediate
+            <input type="radio" name="open_ended_2_3" value="Advanced"> Advanced<br><br>
+
+
+            <label>How well is your GUI development skills?</label><br>
+            <input type="radio" name="open_ended_3_1" value="Beginner"> Beginner
+            <input type="radio" name="open_ended_3_2" value="Intermediate"> Intermediate
+            <input type="radio" name="open_ended_3_3" value="Advanced"> Advanced<br><br>
+
+            <label>How well is your UX/UI design skills?</label><br>
+            <input type="radio" name="open_ended_4_1" value="Beginner"> Beginner
+            <input type="radio" name="open_ended_4_2" value="Intermediate"> Intermediate
+            <input type="radio" name="open_ended_4_3" value="Advanced"> Advanced<br><br>
+
+            <label>How well is your database development skills?</label><br>
+            <input type="radio" name="open_ended_5_1" value="Beginner"> Beginner
+            <input type="radio" name="open_ended_5_2" value="Intermediate"> Intermediate
+            <input type="radio" name="open_ended_5_3" value="Advanced"> Advanced<br><br>
+
+            <h3>Front-End</h3>
+
+            <h3>Back-End</h3>
+
+            <h3>GUI Development</h3>
+            <label>How would you rate your skills in GUI development?</label><br>
+            <input type="radio" name="gui_1_1" value="Weakest"> Weakest
+            <input type="radio" name="gui_1_2" value="Weak"> Weak
+            <input type="radio" name="gui_1_3" value="Neutral"> Neutral
+            <input type="radio" name="gui_1_4" value="Strong"> Strong
+            <input type="radio" name="gui_1_5" value="Strongest"> Strongest<br><br>
+
+            <h3>UX/UI Design</h3>
+            <label>How would you rate your skills in UX/UI design?</label><br>
+            <input type="radio" name="uxui_1_1" value="Weakest"> Weakest
+            <input type="radio" name="uxui_1_2" value="Weak"> Weak
+            <input type="radio" name="uxui_1_3" value="Neutral"> Neutral
+            <input type="radio" name="uxui_1_4" value="Strong"> Strong
+            <input type="radio" name="uxui_1_5" value="Strongest"> Strongest<br><br>
+
+            <h3>Database Development</h3>
+            <label>How would you rate your skills in database development?</label><br>
+            <input type="radio" name="database_1_1" value="Weakest"> Weakest
+            <input type="radio" name="database_1_2" value="Weak"> Weak
+            <input type="radio" name="database_1_3" value="Neutral"> Neutral
+            <input type="radio" name="database_1_4" value="Strong"> Strong
+            <input type="radio" name="database_1_5" value="Strongest"> Strongest<br><br>
+
+            <label>How well can you write code that interacts with a database?</label><br>
+            <input type="radio" name="database_2_1" value="Weakest"> Weakest
+            <input type="radio" name="database_2_2" value="Weak"> Weak
+            <input type="radio" name="database_2_3" value="Neutral"> Neutral
+            <input type="radio" name="database_2_4" value="Strong"> Strong
+            <input type="radio" name="database_2_5" value="Strongest"> Strongest<br><br>
+
+            <h3>Soft Skills</h3>
+
+            <h3>Leadership Skills</h3>
+            <label>I feel comfortable delivering presentations in front of people.</label><br>
+            <input type="radio" name="leadership_1_1" value="Strongly Disagree"> Strongly Disagree
+            <input type="radio" name="leadership_1_2" value="Disagree"> Disagree
+            <input type="radio" name="leadership_1_3" value="Neutral"> Neutral
+            <input type="radio" name="leadership_1_4" value="Agree"> Agree
+            <input type="radio" name="leadership_1_5" value="Strongly Agree"> Strongly Agree<br><br>
+
+            <label>I feel that I can effectively solve a problem utilizing my skill-set.</label><br>
+            <input type="radio" name="leadership_2_1" value="Strongly Disagree"> Strongly Disagree
+            <input type="radio" name="leadership_2_2" value="Disagree"> Disagree
+            <input type="radio" name="leadership_2_3" value="Neutral"> Neutral
+            <input type="radio" name="leadership_2_4" value="Agree"> Agree
+            <input type="radio" name="leadership_2_5" value="Strongly Agree"> Strongly Agree<br><br>
+
+            <label>Have you had any unique experience in internships that you would like to bring to the team?</label><br>
+            <textarea name="leadership_3" rows="4" cols="50"></textarea><br><br>
+
+            <h3>Languages</h3>
+            <label>Please select languages you are proficient in.</label><br>
+            <input type="checkbox" name="languages" value="python"> Python<br>
+            <input type="checkbox" name="languages" value="java"> Java<br>
+            <input type="checkbox" name="languages" value="javascript"> JavaScript<br>
+            <input type="checkbox" name="languages" value="c"> C<br>
+            <input type="checkbox" name="languages" value="c++"> C++<br>
+            <input type="checkbox" name="languages" value="c#"> C#<br>
+            <input type="checkbox" name="languages" value="php"> PHP<br>
+            <input type="checkbox" name="languages" value="html"> HTML<br>
+            <input type="checkbox" name="languages" value="css"> CSS<br>
+            <input type="checkbox" name="languages" value="sql"> SQL<br>
+        </form>
+    </body>
+</html>
