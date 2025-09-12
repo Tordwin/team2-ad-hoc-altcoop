4a536b522d362b0445a00c3b3d791634ab4a4d52
Colby Mitchell <cem1236@g.rit.edu>
2025-09-12 13:51:38 -0400

    Some additions to data_convert.py, some issues with sqlite file

--- address_data_convert.py
+++ address_data_convert.py
@@ -6,19 +6,39 @@
 
 import csv
 import sqlite3
+import math
 
 def get_sqlite_path():
     filepath = input("Enter the absolute path to your SQLite file: ")
-
     return filepath
 
+    # This is gonna determine 1/3 of the original file, so that it can split the data more easily
+def get_rows_per_file(parts_count, cursor):
+    cursor.execute("SELECT COUNT(*) FROM addresses")
+    rows_count = cursor.fetchone()[0]
+    rows_per_file = math.ceil(rows_count/parts_count)
+    return rows_per_file
+
+    # This function will split the larger database file and split it into (tentatively) 3 smaller csv files.
 def sqlite_convert(filepath):
     conn = sqlite3.connect(filepath)
     cursor = conn.cursor()
-    #cont. here later
+
+    # IMPORTANT - This variable determines how many segments this will output
+    # parts_count = 3
+    # rows_per_file = get_rows_per_file(parts_count, cursor)
+
+    cursor.execute("SELECT * FROM addresses")   
+
+    column_names = [name[0] for name in cursor.description]
+    print(column_names)
+    # Stopped temporarily here - issues with sqlite file
+
 
 def main():
-    print(get_sqlite_path())
+    filepath = get_sqlite_path()
+    print(filepath)
+    sqlite_convert(filepath)
 
 if __name__ == "__main__":
     main()
