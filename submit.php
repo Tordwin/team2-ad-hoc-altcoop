28cdf2d3cbc7d712644b01ea7e4d37c4bb4d1c3a
Tordwin <ec7233@g.rit.edu>
2025-09-23 15:01:59 -0400

    successfully connected to database

--- submit.php
+++ submit.php
@@ -2,11 +2,13 @@
     $servername = "localhost";
     $username = "iste498t01";
     $password = "Gras4@overlap";
-    $dbname = "survey";
+    $dbname = "iste498t01";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Connected to database successfully!";
     }
 ?>
